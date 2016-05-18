0aefe18afb9e210d3139252878eb60a65fd47e53
Sam Meech-Ward <sam@meech-ward.com>
2016-05-17 18:31:31 -0700

    Initial Commit

---
+++ Delegate/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Delegate
+//
+//  Created by Sam Meech Ward on 2016-05-17.
+//  Copyright © 2016 meech-ward. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
